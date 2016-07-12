2aa4f7bbaa4b70aad1c97887f150f28faf0973d7
annettekey <askey2020@gmail.com>
2016-07-12 19:25:36 -0400

    good clocks

---
+++ clock.js
@@ -0,0 +1,37 @@
+$ ()
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    var ampm = isAMorPM ();
+    h = correctTime(h);
+    h = checkTime (h);
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =
+    h + ":" + m + ":" + s + ampm;
+    var t = setTimeout(startTime, 500);
+
+
+}
+
+function isAMorPM(i) {
+    if (i >12) {
+        return "pm";
+    } else {
+        return "am";
+    }
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+
+function correctTime(i) {
+    if (i > 12) {
+        i = i - 12;
+
+    }
+    return i;
+}
